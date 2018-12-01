81ea2d87b506df0f8efa2877047e5df6579e9083
tpendergrass <generoustom@gmail.com>
2018-12-01 16:33:24 -0700

    TRAPS FOR EVERYONE

---
+++ Assets/Scripts/DeathTouch.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTouch : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D collision) {
+		Debug.Log("AOUJWEIU");
+		collision.gameObject.SendMessage("Kill", SendMessageOptions.DontRequireReceiver);
+    }
+}
