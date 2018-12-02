8a4e768693faf064f88059ab98177f2569017342
sketchycode <jeremy@sketchyventures.com>
2018-12-02 16:12:54 -0700

    moved a script

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+	public static AudioManager Instance;
+
+	private void Awake() {
+		if(Instance != null) {
+			GameObject.Destroy(gameObject);
+		}
+		else {
+			Instance = this;
+		}
+	}
+
+	public AudioClip test;
+}
