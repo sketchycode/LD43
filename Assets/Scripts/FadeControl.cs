78a719a3e5842f4d8d66c993d9657b4cb659822d
tpendergrass <generoustom@gmail.com>
2018-12-02 13:33:02 -0700

    Add Fade capabilities for level transition

---
+++ Assets/Scripts/FadeControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class FadeControl {
+	public Image fadePanel;
+	public float fadeSpeed;
+
+	private Color black = Color.black;
+	private Color clear = Color.clear;
+
+	private Color placeholderColor;
+
+	public IEnumerator FadeIn() {
+		for(float timer = 0.0f; timer <= 1; timer += Time.deltaTime * fadeSpeed) {
+			Debug.Log("Throwing fade: " + timer);
+			fadePanel.color = Color.Lerp(black, clear, timer);
+			yield return null;
+		}
+	}
+
+	public IEnumerator FadeOut() {
+		for(float timer = 1.0f; timer >= 0.0f; timer += Time.deltaTime * fadeSpeed) {
+			fadePanel.color = Color.Lerp(clear, black, timer);
+			yield return null;
+		}
+	}
+}
