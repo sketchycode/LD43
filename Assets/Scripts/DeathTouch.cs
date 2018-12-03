6a47974c24ed1f41e693713812e4f7904e435ddc
tpendergrass <generoustom@gmail.com>
2018-12-02 20:29:27 -0700

    Added placeholder sound for fire, spikes, and switch

--- Assets/Scripts/DeathTouch.cs
+++ Assets/Scripts/DeathTouch.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DeathTouch : MonoBehaviour {
+	public AudioSource touchSound;
+
 	void OnTriggerEnter2D(Collider2D collision) {
+		if(touchSound) {
+			touchSound.Play();
+		}
 		collision.gameObject.SendMessage("Die", SendMessageOptions.DontRequireReceiver);
     }
 }
